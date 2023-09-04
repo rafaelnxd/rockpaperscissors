e9b7b055bbd8000ed2bc1dc63864c16f21249ca7
rafaelnxd <rafaelnovaes46@outlook.com>
2023-09-04 09:01:55 -0300

    first lines.2

---
+++ rockpaperscissor.py
@@ -0,0 +1,37 @@
+import random
+
+
+user_action = input("Digite a sua escolha (Pedra, Papel ou Tesoura):")
+possible_actions = ['Pedra','Papel','Tesoura']
+computer_action=random.choice(possible_actions)
+print(f"\nVocê escolheu {user_action}, Computador escolheu {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f'Ambos os jogadores selecionara {user_action}, é empate')
+elif user_action == "Pedra":
+    if computer_action == "Tesoura":
+        print(f'Pedra ganha de tesoura, você venceu!')
+    else:
+        print(f'Pedra perde de papel, você perdeu')
+elif user_action == "Papel":
+    if computer_action == "Pedra":
+        print(f'Papel ganha de pedra, você venceu!')
+    else:
+        print(f'Papel perde de tesoura, você perdeu!')
+elif user_action == "Tesoura":
+    if computer_action == "Papel":
+        print(f'Tesoura ganha de papel, você venceu')
+    else:
+        print(f'Tesoura perde de Pedra, você perdeu')
+
+
+        
+
+
+
+
+    
+    
+        
+
+
